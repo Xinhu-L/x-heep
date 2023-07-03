976f4deee0a540707df192fdf54907da1e3f1624
Xinhu <xinhuliu@outlook.com>
2023-07-03 10:35:40 +0200

    on-chip-application

--- sw/applications/on_chip_tinyODIN/main.c
+++ sw/applications/on_chip_tinyODIN/main.c
@@ -14,92 +14,70 @@
 #include "pad_control_regs.h"  // Generated.
 #include "x-heep.h"
 
-#define GPIO_INTR  GPIO_INTR_16
-
 int main(int argc, char *argv[])
 {
-    plic_result_t plic_res;
-    gpio_params_t gpio_params, gpio_ap_params;
-    gpio_t gpio, gpio_ao;
+    gpio_params_t gpio_params;
+    gpio_t gpio;
     
     gpio_result_t gpio_res;
-    gpio_params.base_addr = mmio_region_from_addr((uintptr_t)GPIO_AO_START_ADDRESS);
-    gpio_res = gpio_init(gpio_ap_params, &gpio_ao);
-    if (gpio_res != kGpioOk) {
-        printf("Failed\n;");
-        return -1;
-    }
+    tinyODIN_t tinyODIN;
+
+    tinyODIN.base_addr = mmio_region_from_addr((uintptr_t)TINYODIN_START_ADDRESS);
+    
     gpio_params.base_addr = mmio_region_from_addr((uintptr_t)GPIO_START_ADDRESS);
     gpio_res = gpio_init(gpio_params, &gpio);
-    if (gpio_res != kGpioOk) {
-        printf("Failed\n;");
-        return -1;
-    }
 
-
-    for(int i=0; i<4; i++){
-        gpio_res = gpio_output_set_enabled(&gpio, i+12, true);
-        if (gpio_res != kGpioOk) {
-            printf("Failed\n;");
-            return -1;
-        }
-    }
-
-    for(int i=0; i<4; i++){
+    for(int i=0; i<10; i++){
         gpio_res = gpio_input_enabled(&gpio, i+8, true);
-        if (gpio_res != kGpioOk) {
-            printf("Failed\n;");
-            return -1;
-        }
     }
 
-    bool bit_0, bit_1, bit_2, bit_3;
-    gpio_write(&gpio, 12, false);
-    gpio_write(&gpio, 13, true);
-    gpio_write(&gpio, 14, false);
-    gpio_write(&gpio, 15, true);
-    gpio_read(&gpio, 8, &bit_0);
-    gpio_read(&gpio, 9, &bit_1);
-    gpio_read(&gpio, 10, &bit_2);
-    gpio_read(&gpio, 11, &bit_3);
+    tinyODIN_synaptic_init(tinyODIN);
+    tinyODIN_synaptic_l1_init(tinyODIN);
+    tinyODIN_synaptic_l2_init(tinyODIN);
 
-    bool bit_7, bit_6, bit_5, bit_4;
-    gpio_write(&gpio, 12, false);
-    gpio_write(&gpio, 13, false);
-    gpio_write(&gpio, 14, false);
-    gpio_write(&gpio, 15, true);
-    gpio_read(&gpio, 8, &bit_4);
-    gpio_read(&gpio, 9, &bit_5);
-    gpio_read(&gpio, 10, &bit_6);
-    gpio_read(&gpio, 11, &bit_7);
+    bool bit_7, bit_6, bit_5, bit_4, bit_0, bit_1, bit_2, bit_3;
+    bool one_pixel, exit;
+    int input_spike_one_pixel;
+    int input_spike[144];
+    int frame_count = 0;
+    while(1){
 
-    tinyODIN_t tinyODIN;
-    tinyODIN.base_addr = mmio_region_from_addr((uintptr_t)TINYODIN_START_ADDRESS);
-    printf("Finished :)\n");
+        gpio_read(&gpio, 17, &exit);
+        if(exit == 1){
+            break;
+        }
+        gpio_read(&gpio, 16, &one_pixel);
+        if(one_pixel == 1){
+            printf("One pixel coming...");
+            gpio_read(&gpio, 8, &bit_0);
+            gpio_read(&gpio, 9, &bit_1);
+            gpio_read(&gpio, 10, &bit_2);
+            gpio_read(&gpio, 11, &bit_3);
+            gpio_read(&gpio, 12, &bit_4);
+            gpio_read(&gpio, 13, &bit_5);
+            gpio_read(&gpio, 14, &bit_6);
+            gpio_read(&gpio, 15, &bit_7);
+            input_spike_one_pixel = (bit_7<<7) + (bit_6<<6) + (bit_5<<5) + (bit_4<<4) + (bit_3<<3) + (bit_2<<2) + (bit_1<<1) + (bit_0<<0);
+            input_spike[frame_count] = input_spike_one_pixel;
+            frame_count = frame_count + 1;
+            if(frame_count==144){
+                for(ptrdiff_t i=0x00000000; i<0x00000101; i++){
+                    neuron_addr = i;
+                    neuron_data = 0x0015e000;
+                    tinyODIN_neuron_core_write(&tinyODIN,neuron_addr,neuron_data);
+                }
+                tinyODIN_spike_core_write_call(tinyODIN, input_spike);
+                printf("Inferencing...");
+                uint32_t inference_done;
+                while(inference_done!=0xff000401) {
+                    inference_done = tinyODIN_control_read(&tinyODIN, 0);
+                }
+                printf("Finished :)\n");
+            }
+        }
 
-    int input_spike_check, input_spike;
-    input_spike = (bit_7<<7) + (bit_6<<6) + (bit_5<<5) + (bit_4<<4) + (bit_3<<3) + (bit_2<<2) + (bit_1<<1) + (bit_0<<0);
-    gpio_write(&gpio, 12, true);
-    gpio_write(&gpio, 13, false);
-    gpio_write(&gpio, 14, false);
-    gpio_write(&gpio, 15, false);
-    gpio_read(&gpio, 8, &bit_0);
-    gpio_read(&gpio, 9, &bit_1);
-    gpio_read(&gpio, 10, &bit_2);
-    gpio_read(&gpio, 11, &bit_3);
-    gpio_write(&gpio, 12, true);
-    gpio_write(&gpio, 13, false);
-    gpio_write(&gpio, 14, true);
-    gpio_write(&gpio, 15, true);
-    gpio_read(&gpio, 8, &bit_4);
-    gpio_read(&gpio, 9, &bit_5);
-    gpio_read(&gpio, 10, &bit_6);
-    gpio_read(&gpio, 11, &bit_7);
-    input_spike = input_spike<<8 + (bit_7<<7) + (bit_6<<6) + (bit_5<<5) + (bit_4<<4) + (bit_3<<3) + (bit_2<<2) + (bit_1<<1) + (bit_0<<0); 
-    printf("Input spike is %x", input_spike);
-    tinyODIN_spike_core_write(&tinyODIN, 0, input_spike);
-    input_spike_check = tinyODIN_spike_core_read(&tinyODIN, 0);
-    printf("Read-back spike is %x", input_spike_check);
+    }
+
     printf("---Exit---");
     return EXIT_SUCCESS;
 }
